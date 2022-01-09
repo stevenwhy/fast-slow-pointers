2733451634b4a71c10ccc1a895d8988330dba6a2
stevenwhy <stevenyildirim@gmail.com>
2022-01-09 14:00:25 -0500

    determine if repeatedly getting sum of square of digits gets stuck at 1 or not. Happy number.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,14 +7,72 @@ fun main() {
     head.next!!.next!!.next!!.next = Node(5)
     head.next!!.next!!.next!!.next!!.next = Node(6)
     println("Has cycle: ${linkedListHasCycle(head)} expected false")
-
+    println("~~~~~~~~~~~~~~")
     head.next!!.next!!.next!!.next!!.next!!.next = head.next!!.next
     println("Has cycle: ${linkedListHasCycle(head)} expected true")
     println("Cycle length: ${lengthOfCycle(head)} expected 4")
-
+    println("Start of cycle: ${findStartingNodeOfCycle(head)?.value} expected 3")
+    println("~~~~~~~~~~~~~~")
     head.next!!.next!!.next!!.next!!.next!!.next = head.next!!.next!!.next
     println("Has cycle: ${linkedListHasCycle(head)} expected true")
     println("Cycle length: ${lengthOfCycle(head)} expected 3")
+    println("Start of cycle: ${findStartingNodeOfCycle(head)?.value} expected 4")
+    println("~~~~~~~~~~~~~~")
+    head.next!!.next!!.next!!.next!!.next!!.next = head
+    println("Start of cycle: ${findStartingNodeOfCycle(head)?.value} expected 1)")
+    println("~~~~~~~~~~~~~~")
+    println("Is 23 happy num: ${findHappyNumber(23)} expected true)")
+    //println("Is 12 happy num: ${findHappyNumber(12)} expected false)")
+}
+
+/**
+ * Any number will be called a happy number if,
+ *     after repeatedly replacing it with a number equal to the sum of the square of all of its digits,
+ *     leads us to number ‘1’.
+ */
+fun findHappyNumber(num: Int): Boolean {
+    var slow = num
+    var fast = num
+    while(true) {
+        slow = findSumOfSquareOfDigits(slow)
+        fast = findSumOfSquareOfDigits(findSumOfSquareOfDigits(fast))
+        if(fast == slow) break
+    }
+    return slow == 1
+}
+private fun findSumOfSquareOfDigits(num: Int): Int {
+    var result = 0
+    var temp = num
+    while(temp > 0) {
+        val digit = temp%10
+        result += digit * digit
+        temp /= 10
+    }
+    return result
+}
+
+/**
+ * Given the head of a Singly LinkedList that contains a cycle,
+ *    write a function to find the starting node of the cycle.
+ */
+fun findStartingNodeOfCycle(head: Node): Node? {
+    // we need to make the fastPointer ahead by the length of the cycle,
+    //   then just increment fast and slow by 1 until they meet
+    val length = lengthOfCycle(head)
+    var slow: Node? = head
+    var fast: Node? = head
+    // first increment fast by the length of the cycle
+    for(i in 0 until length) {
+        fast = fast?.next
+    }
+    while(true) {
+        if(fast == slow) return slow
+        if(fast == null || slow == null) return null
+        else {
+            fast = fast.next
+            slow = slow.next
+        }
+    }
 }
 fun lengthOfCycle(head: Node): Int {
     var result = 0
